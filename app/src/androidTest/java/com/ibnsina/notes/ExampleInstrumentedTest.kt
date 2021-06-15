4388b5d081a5a1d625ab3b8e35aacbb54b803e6f
IslamAssem <islamassem95@gmail.com>
2021-06-15 15:21:55 +0200

    add tests

--- app/src/androidTest/java/com/ibnsina/notes/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ibnsina/notes/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.ibnsina.utils
+package com.ibnsina.notes
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ibnsina.utils", appContext.packageName)
+        assertEquals("com.ibnsina.notes", appContext.packageName)
     }
 }
--- usecases/src/test/java/com/ibnsina/usecases/datasource/database/NotesDatabaseTest.kt
+++ usecases/src/test/java/com/ibnsina/usecases/datasource/database/NotesDatabaseTest.kt
@@ -4,10 +4,14 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import com.ibnsina.entites.note.Note
 import junit.framework.TestCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
 import org.junit.*
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -20,11 +24,17 @@ import kotlin.test.assertTrue
 class NotesDatabaseTest {
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
-    private lateinit var notesDatabase: NotesDatabase
 
+    private lateinit var notesDatabase: NotesDatabase
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testScope = TestCoroutineScope(testDispatcher)
 
     @Before
     fun createInMemoryDatabase() {
+
+        // provide the scope explicitly, in this example using a constructor parameter
+        Dispatchers.setMain(testDispatcher)
+
         notesDatabase = Room.inMemoryDatabaseBuilder(RuntimeEnvironment.systemContext,
             NotesDatabase::class.java).allowMainThreadQueries().build()
     }
@@ -34,24 +44,22 @@ class NotesDatabaseTest {
         notesDatabase.close()
     }
 
-    //<editor-fold desc="Save Bufferoos">
     @Test
     fun `saveNote() return id`() {
         val note = Note(1,"title","body","15/06/2021","15/06/2021")
-
-        val id = notesDatabase.notesDao.saveNote(note)
-        assertEquals(id,note.id)
+        testScope.runBlockingTest {
+            val id = notesDatabase.notesDao.saveNote(note)
+            assertEquals(id, note.id)
+        }
     }
-    //<editor-fold desc="Save Bufferoos">
     @Test
     fun `getNote() return savedNote`() {
-        val note = Note(1,"title","body","15/06/2021","15/06/2021")
 
-        val id = notesDatabase.notesDao.saveNote(note)
-        assertEquals(id,note.id)
-        val notes = notesDatabase.notesDao.getAllNotes()
-        runBlockingTest {
-            assertEquals(notes.count(),5)
+        testScope.runBlockingTest {
+            val note = Note(1,"title","body","15/06/2021","15/06/2021")
+            val id = notesDatabase.notesDao.saveNote(note)
+            assertEquals(id,note.id)
+            val notes = notesDatabase.notesDao.getAllNotes()
             assertTrue(notes.first().contains(note))
         }
     }
